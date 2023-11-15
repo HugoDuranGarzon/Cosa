4ef02764fcd171771484d8c4348c1f08a06f6fd4
Administrador <Administrador@10.0.4.49>
2023-11-15 18:36:46 +0100

    cambio3

--- src/cosa/Cosa.java
+++ src/cosa/Cosa.java
@@ -5,6 +5,8 @@ public class Cosa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola a todos");
+		System.out.println("y");
+		System.out.println("Hola a todas");
 
 	}
 
